f2be691ce5cf603f56a05a1cc20a7fca3346bc8f
Rodmul <alex.tyulin@gmail.com>
2022-06-15 11:12:32 +0300

    graceful restart and stop

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"Notebook/logging"
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -21,10 +27,42 @@ func main() {
 	})
 
 	router.GET("/", func(ctx *gin.Context) {
+		time.Sleep(2 * time.Second)
 		for key, value := range info {
-			fmt.Fprint(ctx.Writer, fmt.Sprintf("Заголовок: %s\nТекст: %s\n\n", key, value))
+			fmt.Fprint(ctx.Writer, fmt.Sprintf("Title: %s\nBody: %s\n\n", key, value))
 		}
 	})
 
-	router.Run()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to graceful shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Info("Shutdown Server ... ")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Fatalf("Serer Shutdown: %v", err)
+	}
+	// catching ctx.Done().timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		logger.Info("timeout 5 seconds.")
+	}
+	logger.Info("Server exiting")
 }
